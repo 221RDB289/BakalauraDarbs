13c54b7049d5172698ee53f50cdef6d199a0c2d3
221RDB289 <gundars.pelle@edu.rtu.lv>
2025-04-07 18:55:17 +0300

    testi ar adresēm, koordinātēm un OSM

--- routes.py
+++ routes.py
@@ -2,12 +2,16 @@ import subprocess
 import os
 import shutil
 import sys
+import sumolib
+from geopy.geocoders import Nominatim
+import math
 
 FOLDER = "simulation_files"
 STATIC = "static_files"
 
-if __name__ == "__main__":
-    # ģenerē maršrutus nejaušības gadījumā
+
+# ģenerē maršrutus nejaušības gadījumā
+def create_test_routes():
     if not os.path.exists(f"{FOLDER}/riga.trips.xml"):
         cmd = [
             "python",
@@ -39,3 +43,86 @@ if __name__ == "__main__":
     else:
         print("ERROR: SUMO is not installed")
         sys.exit()
+
+
+# pārveido doto adresi uz tuvāko "node"
+def address_to_node(
+    address,
+    geolocator,
+    net,
+):
+    location = geolocator.geocode(address)
+    x, y = net.convertLonLat2XY(location.longitude, location.latitude)
+    print(x, y)
+    return x, y
+    # node = net.getClosestNode(x, y)
+    # return node.id
+
+
+# edges = net.getNeighboringEdges(x, y, radius)
+
+
+# no koordinātēm iegūst tuvāko tīkla "node" pēc Eiklīda attāluma:
+def get_closest_node(coordinates, distance, net):
+    x, y = coordinates
+    for node in net.getNodes():
+        if node:
+            xn, yn = node.getCoord()
+            if math.sqrt((x - xn) ** 2 + (y - yn) ** 2) <= distance:
+                print(xn, yn)
+                return node.getID()
+    return
+
+
+if __name__ == "__main__":
+    # create_test_routes()
+
+    net = sumolib.net.readNet(f"{FOLDER}/riga.net.xml")
+    radius = 500
+
+    x = 3683.9117356822244
+    y = 7678.589846183546
+
+    # x, y = net.convertLonLat2XY(lon, lat)
+    edges = net.getNeighboringEdges(x, y, radius)
+    if edges:
+        distancesAndEdges = sorted(
+            [(dist, edge) for edge, dist in edges], key=lambda x: x[0]
+        )
+        dist, closestEdge = distancesAndEdges[0]
+        print(closestEdge)
+
+    # distance = 500
+
+    # geolocator = Nominatim(user_agent="sumo_routing")
+    # net = sumolib.net.readNet(f"{FOLDER}/riga.net.xml")
+
+    # address1 = "Plieņciema iela 35, Mārupe, Mārupes pagasts, Mārupes novads, LV-2167"
+    # address2 = "Zunda krastmala 10, Kurzemes rajons, Rīga, LV-1048"
+
+    # node1 = get_closest_node(address_to_node(address=address1, geolocator=geolocator, net=net),distance, net)
+
+    # node2 = get_closest_node(address_to_node(address=address2, geolocator=geolocator, net=net),distance, net)
+
+    # print(f"Closest SUMO nodes: {node1}, {node2}")
+
+    # net = sumolib.net.readNet(f"{FOLDER}/riga.net.xml")
+    # for node in net.getNodes():
+    #     xn,yn = node.getCoord()
+    #     if math.sqrt((x-xn)**2+(y-yn)**2)
+    #     print(node.getID())
+
+    # location1 = geolocator.geocode(address1)
+    # location2 = geolocator.geocode(address2)
+
+    # print(f"Address 1: {location1.latitude}, {location1.longitude}")
+    # print(f"Address 2: {location2.latitude}, {location2.longitude}")
+
+    # net = sumolib.net.readNet(f"{FOLDER}/riga.net.xml")
+    # x1, y1 = net.convertLonLat2XY(location1.longitude, location1.latitude)
+    # x2, y2 = net.convertLonLat2XY(location2.longitude, location2.latitude)
+
+    # node1 = net.getClosestNode(x1, y1)
+    # node2 = net.getClosestNode(x2, y2)
+
+    # print(f"Closest SUMO nodes: {node1.id}, {node2.id}")
---
+++ test.py
@@ -0,0 +1,53 @@
+import overpy
+from geopy.geocoders import Nominatim
+from db import *
+
+"""
+iegūst koorinātes no adreses:
+https://operations.osmfoundation.org/policies/nominatim/
+- maksimāli ir atļauts 1 pieprasījums 1 sekundē
+- rezultātus vajag saglabāt, lai pieprasījumus nevajadzētu atkārtot
+"""
+
+
+def address_to_coordinates(geolocator, address):
+    # vai šis adreses pieprasījums (vai šī adrese) jau ir datubāzē:
+    sql = f"SELECT latitude,longitude FROM locations WHERE address == '{address}';"
+    results = db_get(sql)
+    if results:
+        return results[0]
+    # ja nav ieraksts datubāzē:
+    else:
+        location = geolocator.geocode(address, country_codes=["LV"])
+        lat, long = location.latitude, location.longitude
+        # ievieto jaunu ierakstu datubāzē:
+        sql = f"INSERT INTO locations (address,latitude,longitude) VALUES{address,lat,long};"
+        db_update(sql)
+        return lat, long
+
+
+if __name__ == "__main__":
+    geolocator = Nominatim(user_agent="221RDB289")
+    lat, long = address_to_coordinates(geolocator, "Plieņciema iela 35")
+    print(lat, long)
+
+    overpass = overpy.Overpass()
+
+    """
+    no koordinātēm iegūst tuvāko objektu OSM mapē:
+    - "around" vērtība ir rādius metros
+    """
+    # result = overpass.query(
+    #     f"""
+    # way(around:10,{lat},{long});
+    # out body;
+    # """
+    # )
+
+    result = overpass.query(
+        f"""
+    rel(around:50,{lat},{long});
+    out body;
+    """
+    )
+    print(result.ways)
